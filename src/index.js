1f308418535118fd0b449e7ab47b1a224e06f1f1
Ayush-kr-shanu <aksmilestone35@gmail.com>
2024-09-11 12:41:26 +0530

    update the

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
 const dbConnection = require("./config/db");
 const config = require("./config/config");
 const router = require("./routes")
@@ -18,11 +21,26 @@ app.get("/", (request, response) => {
 app.use("/api", router);
 
 
-app.listen(config.PORT, () => {
-    try {
-        dbConnection()
-        console.log(`Server is running on port ${config.PORT}`);
-    } catch (error) {
+// app.listen(config.PORT, () => {
+//     try {
+//         dbConnection()
+//         console.log(`Server is running on port ${config.PORT}`);
+//     } catch (error) {
         
-    }
-})
+//     }
+// })
+
+const keyPath = path.resolve(__dirname, '../selfsigned.key');
+const certPath = path.resolve(__dirname, '../selfsigned.crt');
+
+const options = {
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath)
+};
+
+https.createServer(options, app).listen(443, '0.0.0.0', () => {
+    console.log('Server running with SSL on port 443');
+
+    // Connect to the database after server starts
+    dbConnection();
+});
