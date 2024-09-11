b3854311468258c68b873264d7e1e135aa5e1665
Ayush-kr-shanu <aksmilestone35@gmail.com>
2024-09-11 11:37:52 +0530

    reverted

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const fs = require('fs');
-const path = require('path');
-const https = require('https');
 const dbConnection = require("./config/db");
 const config = require("./config/config");
 const router = require("./routes")
@@ -20,24 +17,12 @@ app.get("/", (request, response) => {
 
 app.use("/api", router);
 
-const keyPath = path.resolve(__dirname, '../selfsigned.key');
-const certPath = path.resolve(__dirname, '../selfsigned.cert');
 
-const options = {
-    key: fs.readFileSync(keyPath),
-    cert: fs.readFileSync(certPath)
-};
-
-https.createServer(options, app).listen(443, '0.0.0.0', () => {
-    dbConnection()
-    console.log('Server running with SSL on port 443');
-});
-
-// app.listen(config.PORT, () => {
-//     try {
-//         dbConnection()
-//         console.log(`Server is running on port ${config.PORT}`);
-//     } catch (error) {
+app.listen(config.PORT, () => {
+    try {
+        dbConnection()
+        console.log(`Server is running on port ${config.PORT}`);
+    } catch (error) {
         
-//     }
-// })
+    }
+})
